4fea0e420f4f58add101f79eee887f4b3eff489d
= <=>
2023-04-11 02:06:48 -0400

    Added Accoridon Body on the About page

---
+++ frontend/src/components/pages/AboutUs.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+export default function About(props) {
+
+  let myStyle = {
+    color : props.mode==='light'?'black':'white',
+    backgroundColor : props.mode==='light'?'white':'#2f2868',
+    border : '2px solid',
+    borderColor : props.mode==='light'?'black':'white'
+    
+  }
+  return (
+    <div className="container my-3" >
+        <h2 style={{color: props.mode==='light'?'black':'white'}}>About Us</h2>
+      <div className="accordion" id="accordionPanelsStayOpenExample">
+        <div className="accordion-item" >
+          <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseOne"
+              aria-expanded="true"
+              aria-controls="panelsStayOpen-collapseOne"
+              style={myStyle}
+              >
+              Analyse Text
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="panelsStayOpen-headingOne">
+            <div className="accordion-body">
+              <strong>This is the first item's accordion body.</strong> It is
+              the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item" >
+          <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseTwo"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseTwo"
+              style={myStyle}>
+              Free to Use
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="panelsStayOpen-headingTwo"
+          >
+            <div className="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is
+              e <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item" >
+          <h2 className="accordion-header" id="panelsStayOpen-headingThree">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseThree"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseThree"
+              style={myStyle}>
+              Browser Compatible
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="panelsStayOpen-headingThree"
+          >
+            <div className="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
+             <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
